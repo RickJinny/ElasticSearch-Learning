ce167625de78dc29a08d32b9b9fb8b804097a63b
RickJinny <rickjinny@126.com>
2020-01-18 18:29:27 +0800

    add get
    add get

--- ElasticSearch-Basic-Demo/src/main/java/com/rickjin/es/demo/StudentDemo.java
+++ ElasticSearch-Basic-Demo/src/main/java/com/rickjin/es/demo/StudentDemo.java
@@ -1,6 +1,7 @@
 package com.rickjin.es.demo;
 
 
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
@@ -8,21 +9,22 @@ import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 
-import java.io.IOException;
 import java.net.InetAddress;
 
 public class StudentDemo {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         // 先构建
         Settings settings = Settings.builder()
-                                    .put("cluster.name", "es")
+                                    .put("cluster.name", "elasticsearch")
                                     .build();
         // 创建 TransportClient 对象
         TransportClient transportClient = new PreBuiltTransportClient(settings)
                 .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
         // 创建一个学生信息
-        createStudent(transportClient);
+//        createStudent(transportClient);
+        // 获取学生信息
+        getStudent(transportClient);
         transportClient.close();
     }
 
@@ -30,7 +32,7 @@ public class StudentDemo {
      * 创建学生信息（创建一个 document）
      * @param client
      */
-    private static void createStudent(TransportClient client) throws IOException {
+    private static void createStudent(TransportClient client) throws Exception {
         IndexResponse response = client.prepareIndex("school", "student", "1")
                 .setSource(XContentFactory.jsonBuilder()
                         .startObject()
@@ -40,5 +42,15 @@ public class StudentDemo {
                         .endObject())
                 .get();
         System.out.println(response.getResult());
+    }
+
+    /**
+     * 获取员工信息
+     * @param client
+     * @throws Exception
+     */
+    private static void getStudent(TransportClient client) throws Exception {
+        GetResponse response = client.prepareGet("school", "student", "1").get();
+        System.out.println(response.getSourceAsString());
     }
 }
