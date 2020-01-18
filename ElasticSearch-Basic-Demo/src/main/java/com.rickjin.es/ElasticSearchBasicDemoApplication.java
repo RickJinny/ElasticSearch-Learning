e794a1f274e1da5caf786cbb6e26a4543c23a090
RickJinny <rickjinny@126.com>
2020-01-18 17:29:13 +0800

    es basic demo
    es basic demo

---
+++ ElasticSearch-Basic-Demo/src/main/java/com.rickjin.es/ElasticSearchBasicDemoApplication.java
@@ -0,0 +1,12 @@
+package com.rickjin.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticSearchBasicDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticSearchBasicDemoApplication.class, args);
+    }
+}
