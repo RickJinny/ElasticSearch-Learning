5216fdba53cbf1c3e1fd786106452fb03afe2133
RickJinny <rickjinny@126.com>
2020-01-18 17:52:25 +0800

    update
    update

---
+++ ElasticSearch-Basic-Demo/src/main/java/com/rickjin/es/demo/ElasticSearchDemo.java
@@ -0,0 +1,11 @@
+package com.rickjin.es.demo;
+
+
+import org.elasticsearch.common.settings.Settings;
+
+public class ElasticSearchDemo {
+
+    public static void main(String[] args) {
+        // 先构建
+    }
+}
