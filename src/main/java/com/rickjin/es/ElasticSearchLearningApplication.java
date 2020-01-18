eaa88008baea415c132def55e22f7ac08cd8f4bf
RickJinny <rickjinny@126.com>
2020-01-18 17:24:21 +0800

    update
    update

---
+++ src/main/java/com/rickjin/es/ElasticSearchLearningApplication.java
@@ -0,0 +1,13 @@
+package com.rickjin.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticSearchLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticSearchLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rickjin/es/ElasticSearchLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rickjin.es;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticSearchLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
